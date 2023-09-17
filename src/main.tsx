9713ff5ffc6bdb841f94dcd18bcee20abaedc4be
Shiloh George <shilohgeorge2019@gmail.com>
2023-09-17 16:54:46 +0100

    updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import 'react-toastify/ReactToastify.css';
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<AnimatePresence mode='wait'>
 			<ContextProvider>
